2fde2c2ee0c23112794bdf330ae6b5b9753c93e1
Paradox <1178929518@qq.com>
2021-04-30 15:52:18 +0800

    v0.1.0

---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+use std::fs;
+use std::error::Error;
+
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    let result=if config.case_sensitive {
+        search(&config.query,&contents)
+    }else{
+        search_case_sensitive(&config.query,&contents)
+    };
+    for line in result {
+        println!("{}", line);
+    }
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive:bool,
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+pub fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        let case_sensitive=std::env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config { query, filename,case_sensitive })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "Rust:\nsafe,fast,productive.\npick three.\n";
+
+        assert_eq!(vec!["safe,fast,productive."], search(query, contents));
+    }
+}
