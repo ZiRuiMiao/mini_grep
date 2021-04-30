b5ac07fda7bf2dcff05e013ebb6372b1d6508587
Paradox <1178929518@qq.com>
2021-04-30 16:01:30 +0800

    v0.1.0 rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 
-use std::process;
 use grep::Config;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,12 +9,10 @@ fn main() {
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem arguments :{}", err);
         process::exit(1);
-    }
-    );
+    });
 
     if let Err(e) = grep::run(config) {
         println!("Application error:{}", e);
         process::exit(1);
     }
 }
-
